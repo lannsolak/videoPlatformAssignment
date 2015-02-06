6b5413bbc0534eb0407f273cb0eaeafdcc7b0601
Solak LANN <slan1@we-bridge.net>
2015-02-06 13:31:46 +0700

    header and home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,32 +4,10 @@ class Home extends V_Controller {
 	public function __construct() {
         parent::__construct();
     }
-
 	public function index()
 	{
 		$this->load->view('index');
 	}
-
-	public function about(){
-		$this->load->view('home/about');
-	}
-
-	function home(){
-		$this->load->view('index');
-	}
-
-	function recentlyall(){
-		$this->load->view('home/recentlyall');
-	}
-
-	function popularall(){
-		$this->load->view('home/popularall');
-	}
-
-	function competitionall(){
-		$this->load->view('home/competitionall');
-	}
-
 }
 
 /* End of file home.php */
--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -27,38 +27,51 @@
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
+            <div class="navbar-header col-sm-2">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Contest Online Video Platform <?php // echo img("assets/img/covp-logo.png"); ?></a>
+                <a class="navbar-brand" href="<?php echo base_url(); ?>">CORP <?php // echo img("assets/img/covp-logo.png"); ?></a>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
+
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
+                <div class="col-sm-7">
+                    <center>
+                        <ul class="nav navbar-nav">
+                            <li>
+                                <a href="<?php echo base_url(); ?>"><span class="glyphicon glyphicon-home"></span> </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url(); ?>">About Us</a>
+                            </li>
+                            <li>
+                                <?php echo anchor("contest","Contests"); ?>
+                            </li>
+                            <li>
+                                <?php echo anchor("video","Browse Videos"); ?>
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url(); ?>">Contact Us</a>
+                            </li>
+                        </ul>
+                    </center>
+                </div>
+                <div class="col-sm-3">
+                    <div class="input-group row">
+                        <input class="form-control" type="text">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                        </span>
+                    </div>
+                </div>
             </div>
             <!-- /.navbar-collapse -->
-            <!-- <div class="input-group col-sm-4">
-                <input class="form-control" type="text">
-                <span class="input-group-btn">
-                    <button class="btn btn-default" type="button">
-                        <span class="glyphicon glyphicon-search"></span>
-                </button>
-                </span>
-            </div> -->
         </div>
-    </nav>
+    </div>
+</nav>
 

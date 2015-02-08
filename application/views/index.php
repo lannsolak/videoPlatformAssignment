83f62f5b5b80bac12e3d4ff4fb1570f5f94d7f0b
unknown <sim.sokeara@gmail.com>
2015-02-08 15:21:23 +0700

    Sokeara have update Header and register page

--- application/views/index.php
+++ application/views/index.php
@@ -2,13 +2,15 @@
 $this->load->view('inc/header');
 
 $uri = $this->uri->segment(1);
-
-if($uri != ""){
-    $this->load->view($uri.'/'.$uri);
+if(isset($page)){
+	$this->load->view($page);
 }else{
-    $this->load->view('home/home');
+	if($uri != ""){
+	    $this->load->view($uri.'/'.$uri);
+	}else{
+	    $this->load->view('home/home');
+	}
 }
-
 
 $this->load->view('inc/footer');
 ?>
